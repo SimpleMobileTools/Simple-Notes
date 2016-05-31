a28ed90bc6f140025c22b432e856cbab1da4aada
tibbi <tibor@kaputa.sk>
2016-05-31 17:26:01 +0200

    open the app at widget click

--- app/src/main/java/com/simplemobiletools/notes/MyWidgetProvider.java
+++ app/src/main/java/com/simplemobiletools/notes/MyWidgetProvider.java
@@ -1,19 +1,21 @@
 package com.simplemobiletools.notes;
 
+import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.widget.RemoteViews;
 
 public class MyWidgetProvider extends AppWidgetProvider {
     private static SharedPreferences prefs;
+    private RemoteViews remoteViews;
 
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
-        prefs = context.getSharedPreferences(Constants.PREFS, Context.MODE_PRIVATE);
-        final RemoteViews remoteViews = new RemoteViews(context.getPackageName(), R.layout.widget);
+        initVariables(context);
         final int defaultColor = context.getResources().getColor(R.color.dark_grey);
         final int newBgColor = prefs.getInt(Constants.WIDGET_BG_COLOR, defaultColor);
         final int newTextColor = prefs.getInt(Constants.WIDGET_TEXT_COLOR, Color.WHITE);
@@ -24,6 +26,18 @@ public class MyWidgetProvider extends AppWidgetProvider {
             updateWidget(appWidgetManager, widgetId, remoteViews);
         }
         super.onUpdate(context, appWidgetManager, appWidgetIds);
+    }
+
+    private void initVariables(Context context) {
+        prefs = context.getSharedPreferences(Constants.PREFS, Context.MODE_PRIVATE);
+        remoteViews = new RemoteViews(context.getPackageName(), R.layout.widget);
+        setupIntent(R.id.notes_holder, context);
+    }
+
+    private void setupIntent(int id, Context context) {
+        final Intent intent = new Intent(context, MainActivity.class);
+        final PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
+        remoteViews.setOnClickPendingIntent(id, pendingIntent);
     }
 
     private void updateWidget(AppWidgetManager widgetManager, int widgetId, RemoteViews remoteViews) {
