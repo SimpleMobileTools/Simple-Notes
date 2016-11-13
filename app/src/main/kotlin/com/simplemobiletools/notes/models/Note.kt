d32c68c6eeebe7f896afd801eecea5d52978190c
tibbi <tibor@kaputa.sk>
2016-11-14 00:03:06 +0100

    update the Note equals function parameter name

--- app/src/main/kotlin/com/simplemobiletools/notes/models/Note.kt
+++ app/src/main/kotlin/com/simplemobiletools/notes/models/Note.kt
@@ -2,7 +2,7 @@ package com.simplemobiletools.notes.models
 
 class Note(var id: Int, var title: String, var value: String) {
 
-    override fun equals(o: Any?) = o != null && this.toString() == o.toString()
+    override fun equals(other: Any?) = other != null && this.toString() == other.toString()
 
     override fun toString() = "Note {id=$id, title=$title, value=$value}"
 }
