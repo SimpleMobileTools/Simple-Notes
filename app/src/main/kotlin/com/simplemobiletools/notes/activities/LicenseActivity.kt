e5d02f5892f21ae42bd92a41deab1924386fba06
tibbi <tibor@kaputa.sk>
2016-11-13 23:51:45 +0100

    convert License to kotlin

---
+++ app/src/main/kotlin/com/simplemobiletools/notes/activities/LicenseActivity.kt
@@ -0,0 +1,23 @@
+package com.simplemobiletools.notes.activities
+
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import com.simplemobiletools.notes.R
+import kotlinx.android.synthetic.main.activity_license.*
+
+class LicenseActivity : SimpleActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_license)
+        license_butterknife_title.setOnClickListener { openUrl(R.string.butterknife_url) }
+        license_ambilwarna_title.setOnClickListener { openUrl(R.string.ambilwarna_url) }
+    }
+
+    private fun openUrl(id: Int) {
+        val url = resources.getString(id)
+        val browserIntent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
+        startActivity(browserIntent)
+    }
+}
