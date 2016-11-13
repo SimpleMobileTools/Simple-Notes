4fa58b11c9c031b40b31aebcb04df594a0e79b4a
tibbi <tibor@kaputa.sk>
2016-11-13 23:34:57 +0100

    convert App to kotlin

---
+++ app/src/main/kotlin/com/simplemobiletools/notes/App.kt
@@ -0,0 +1,12 @@
+package com.simplemobiletools.notes
+
+import android.app.Application
+
+import com.facebook.stetho.Stetho
+
+class App : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        Stetho.initializeWithDefaults(this)
+    }
+}
