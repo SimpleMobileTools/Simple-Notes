c6b0934b8508469a50bc6915ebf1165be335ab3b
tibbi <tibor@kaputa.sk>
2017-07-04 19:52:46 +0200

    initialize Stetho only in debug build

--- app/src/main/kotlin/com/simplemobiletools/notes/App.kt
+++ app/src/main/kotlin/com/simplemobiletools/notes/App.kt
@@ -1,12 +1,12 @@
 package com.simplemobiletools.notes
 
 import android.app.Application
-
 import com.facebook.stetho.Stetho
 
 class App : Application() {
     override fun onCreate() {
         super.onCreate()
-        Stetho.initializeWithDefaults(this)
+        if (BuildConfig.DEBUG)
+            Stetho.initializeWithDefaults(this)
     }
 }
