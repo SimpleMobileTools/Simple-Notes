1ef3528a622db6f40430c5dd97118aff5442ee6d
tibbi <tibor@kaputa.sk>
2017-11-10 15:32:34 +0100

    adding Leakcanary

--- app/src/main/kotlin/com/simplemobiletools/notes/App.kt
+++ app/src/main/kotlin/com/simplemobiletools/notes/App.kt
@@ -2,10 +2,19 @@ package com.simplemobiletools.notes
 
 import android.app.Application
 import com.facebook.stetho.Stetho
+import com.simplemobiletools.notes.BuildConfig.USE_LEAK_CANARY
+import com.squareup.leakcanary.LeakCanary
 
 class App : Application() {
     override fun onCreate() {
         super.onCreate()
+        if (USE_LEAK_CANARY) {
+            if (LeakCanary.isInAnalyzerProcess(this)) {
+                return
+            }
+            LeakCanary.install(this)
+        }
+
         if (BuildConfig.DEBUG)
             Stetho.initializeWithDefaults(this)
     }
