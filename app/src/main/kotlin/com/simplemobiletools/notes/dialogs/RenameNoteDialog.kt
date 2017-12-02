e834a0646754570070d2ed0f12cf03e2e41e6625
tibbi <tibor@kaputa.sk>
2017-12-02 20:21:48 +0100

    update commons to 3.0.23

--- app/src/main/kotlin/com/simplemobiletools/notes/dialogs/RenameNoteDialog.kt
+++ app/src/main/kotlin/com/simplemobiletools/notes/dialogs/RenameNoteDialog.kt
@@ -22,41 +22,42 @@ class RenameNoteDialog(val activity: SimpleActivity, val db: DBHelper, val note:
                 .setNegativeButton(R.string.cancel, null)
                 .create().apply {
             window!!.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_VISIBLE)
-            activity.setupDialogStuff(view, this, R.string.rename_note)
-            getButton(BUTTON_POSITIVE).setOnClickListener {
-                val title = view.note_name.value
-                when {
-                    title.isEmpty() -> activity.toast(R.string.no_title)
-                    db.doesTitleExist(title) -> activity.toast(R.string.title_taken)
-                    else -> {
-                        note.title = title
-                        val path = note.path
-                        if (path.isNotEmpty()) {
-                            if (title.isEmpty()) {
-                                activity.toast(R.string.filename_cannot_be_empty)
-                                return@setOnClickListener
-                            }
+            activity.setupDialogStuff(view, this, R.string.rename_note) {
+                getButton(BUTTON_POSITIVE).setOnClickListener {
+                    val title = view.note_name.value
+                    when {
+                        title.isEmpty() -> activity.toast(R.string.no_title)
+                        db.doesTitleExist(title) -> activity.toast(R.string.title_taken)
+                        else -> {
+                            note.title = title
+                            val path = note.path
+                            if (path.isNotEmpty()) {
+                                if (title.isEmpty()) {
+                                    activity.toast(R.string.filename_cannot_be_empty)
+                                    return@setOnClickListener
+                                }
 
-                            val file = File(path)
-                            val newFile = File(file.parent, title)
-                            if (!newFile.name.isAValidFilename()) {
-                                activity.toast(R.string.invalid_name)
-                                return@setOnClickListener
-                            }
+                                val file = File(path)
+                                val newFile = File(file.parent, title)
+                                if (!newFile.name.isAValidFilename()) {
+                                    activity.toast(R.string.invalid_name)
+                                    return@setOnClickListener
+                                }
 
-                            activity.renameFile(file, newFile) {
-                                if (it) {
-                                    note.path = newFile.absolutePath
-                                    db.updateNotePath(note)
-                                } else {
-                                    activity.toast(R.string.rename_file_error)
-                                    return@renameFile
+                                activity.renameFile(file, newFile) {
+                                    if (it) {
+                                        note.path = newFile.absolutePath
+                                        db.updateNotePath(note)
+                                    } else {
+                                        activity.toast(R.string.rename_file_error)
+                                        return@renameFile
+                                    }
                                 }
                             }
+                            db.updateNoteTitle(note)
+                            dismiss()
+                            callback(note)
                         }
-                        db.updateNoteTitle(note)
-                        dismiss()
-                        callback(note)
                     }
                 }
             }
