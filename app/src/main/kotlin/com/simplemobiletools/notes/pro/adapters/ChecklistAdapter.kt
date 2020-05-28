879c2b34d1b9b45badbea74b377c43170cf488b9
tibbi <tibor@kaputa.sk>
2020-05-28 22:04:11 +0200

    add bigger margins at the settings section labels

--- app/src/main/kotlin/com/simplemobiletools/notes/pro/adapters/ChecklistAdapter.kt
+++ app/src/main/kotlin/com/simplemobiletools/notes/pro/adapters/ChecklistAdapter.kt
@@ -104,7 +104,7 @@ class ChecklistAdapter(activity: BaseSimpleActivity, var items: ArrayList<Checkl
 
     private fun initDrawables() {
         val res = activity.resources
-        crossDrawable = res.getColoredDrawableWithColor(R.drawable.ic_cross_vector, res.getColor(R.color.theme_dark_red_primary_color))
+        crossDrawable = res.getColoredDrawableWithColor(R.drawable.ic_cross_vector, res.getColor(R.color.md_red_700))
         checkDrawable = res.getColoredDrawableWithColor(R.drawable.ic_check_vector, res.getColor(R.color.md_green_700))
     }
 
