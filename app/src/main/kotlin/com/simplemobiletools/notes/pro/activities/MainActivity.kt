f79ce33d92b36d00ff339e22e3bfb8ed83de2185
tibbi <tibor@kaputa.sk>
2022-03-08 19:06:27 +0100

    do not show some FAQ items at the prepaid variant

--- app/src/main/kotlin/com/simplemobiletools/notes/pro/activities/MainActivity.kt
+++ app/src/main/kotlin/com/simplemobiletools/notes/pro/activities/MainActivity.kt
@@ -626,11 +626,14 @@ class MainActivity : SimpleActivity() {
 
         val faqItems = arrayListOf(
             FAQItem(R.string.faq_1_title_commons, R.string.faq_1_text_commons),
-            FAQItem(R.string.faq_1_title, R.string.faq_1_text),
-            FAQItem(R.string.faq_2_title_commons, R.string.faq_2_text_commons),
-            FAQItem(R.string.faq_6_title_commons, R.string.faq_6_text_commons),
-            FAQItem(R.string.faq_7_title_commons, R.string.faq_7_text_commons)
+            FAQItem(R.string.faq_1_title, R.string.faq_1_text)
         )
+
+        if (!resources.getBoolean(R.bool.hide_google_relations)) {
+            faqItems.add(FAQItem(R.string.faq_2_title_commons, R.string.faq_2_text_commons))
+            faqItems.add(FAQItem(R.string.faq_6_title_commons, R.string.faq_6_text_commons))
+            faqItems.add(FAQItem(R.string.faq_7_title_commons, R.string.faq_7_text_commons))
+        }
 
         startAboutActivity(R.string.app_name, licenses, BuildConfig.VERSION_NAME, faqItems, true)
     }
